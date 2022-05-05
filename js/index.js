7bcba6fd0387d7eb59400c173b3a80eced0786d0
Harveer Hans <harveerhans59@gmail.com>
2022-05-04 23:04:51 -0400

    all views completed

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//get the values from the page
+//starts or controller function
+function getValues(){
+    //get values from the page
+
+    //we call generateNumbers
+
+    //we call displayNumbers
+}
+
+//generate numbers from start value to the endvalue
+//logic function
+function generateNumbers(){
+
+}
+
+//display the numbers and mark even numbers bold
+//display or view function
+function displayNumbers(){
+
+}
